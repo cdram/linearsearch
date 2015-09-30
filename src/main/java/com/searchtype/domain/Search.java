fa920dab0f99f95e13d65a4042afeb23b2a738c5
shriram <shriram.r.ramamurthy@gmail.com>
2015-09-30 14:20:06 -0700

    added constants file to handle string literals

--- src/main/java/com/searchtype/domain/Search.java
+++ src/main/java/com/searchtype/domain/Search.java
@@ -1,11 +1,13 @@
 package com.searchtype.domain;
 
+import com.searchtype.utils.Constants;
+
 import java.util.Comparator;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-/**
+/*
  * Created with IntelliJ IDEA.
  * User: shriram
  */
@@ -22,6 +24,12 @@ public class Search {
         this.ascending = ascending;
     }
 
+    /**
+     * Checks whether a particular object is within the bounds of the list
+     * @param key
+     * @param <T>
+     * @return
+     */
     public <T extends Comparable<T>> Boolean isInBounds(T key){
         T low = (ascending)?(T) list.get(0):(T) list.get(nItems-1);
         T high = (ascending)?(T) list.get(nItems-1):(T) list.get(0);
@@ -38,6 +46,19 @@ public class Search {
     private <T extends Comparable<T>> SearchResult getSearchResult(T indexItem, T key){
         int compareValue = key.compareTo(indexItem);
         return (compareValue == 0)?SearchResult.FoundExact:(compareValue < 0)?SearchResult.FoundGreater:SearchResult.FoundLess;
+    }
+
+    /**
+     * Helper Method to set the helper map with the search constant and the decrement index
+     * @param map
+     * @param searchConstant
+     * @param decrementIndex
+     * @return
+     */
+    private Map setMap(Map map, int searchConstant, int decrementIndex){
+        map.put(Constants.SEARCH_CONSTANT, searchConstant);
+        map.put(Constants.DECREMENT_INDEX, decrementIndex);
+        return map;
     }
 
 
@@ -58,58 +79,27 @@ public class Search {
         switch (searchType){
             case LessThan:
                 comparator = (o1, o2) -> {return  (o1.compareTo(o2) < 0)?0:-1; };
-                if(ascending){
-                    map.put("searchConstant", 0);
-                    map.put("subtract", 1);
-                }else{
-                    map.put("searchConstant", -1);
-                    map.put("subtract", 0);
-                }
-
+                map = (ascending)?setMap(map, 0,1):setMap(map, -1,0);
                 break;
 
             case LessThanEquals:
                 comparator = (o1, o2) -> {return  (o1.compareTo(o2) <= 0)?0:-1; };
-                if(ascending){
-                    map.put("searchConstant", 0);
-                    map.put("subtract", 1);
-                }else{
-                    map.put("searchConstant", -1);
-                    map.put("subtract", 0);
-                }
+                map = (ascending)?setMap(map, 0,1):setMap(map, -1,0);
                 break;
 
             case Equals:
                 comparator = (o1, o2) -> {return  (o1.compareTo(o2) == 0)?0:-1; };
-                if(ascending){
-                    map.put("searchConstant", -1);
-                    map.put("subtract", 0);
-                }else{
-                    map.put("searchConstant", -1);
-                    map.put("subtract", 0);
-                }
+                map = (ascending)?setMap(map, -1,0):setMap(map, -1,0);
                 break;
 
             case GreaterThanEquals:
                 comparator = (o1, o2) -> {return  (o1.compareTo(o2) >= 0)?0:-1; };
-                if(ascending){
-                    map.put("searchConstant", -1);
-                    map.put("subtract", 0);
-                }else{
-                    map.put("searchConstant", 0);
-                    map.put("subtract", 1);
-                }
+                map = (ascending)?setMap(map, -1,0):setMap(map, 0,1);
                 break;
 
             case GreaterThan:
                 comparator = (o1, o2) -> {return  (o1.compareTo(o2) > 0)?0:-1; };
-                if(ascending){
-                    map.put("searchConstant", -1);
-                    map.put("subtract", 0);
-                }else{
-                    map.put("searchConstant", 0);
-                    map.put("subtract", 1);
-                }
+                map = (ascending)?setMap(map, -1,0):setMap(map, 0,1);
                 break;
 
             default:
@@ -117,10 +107,17 @@ public class Search {
                 break;
         }
 
-        map.put("comparator", comparator);
+        map.put(Constants.COMPARATOR, comparator);
         return map;
     }
 
+    /*
+     * Perform the Linear Search based on the SearchType and the Key
+     * @param searchType
+     * @param key
+     * @param <T>
+     * @return
+     */
     public <T extends Comparable<T>> Result search(SearchType searchType, T key){
         Result result = null;
         int i = 0;
@@ -128,19 +125,24 @@ public class Search {
         //get the parameter map which we will use for making search decisions
         Map map = getParameters(searchType);
 
-        Comparator<T> comparator = (Comparator<T>) map.get("comparator");
-        int searchConstant = (int) map.get("searchConstant");
+        //fetch the comparator for the corresponding SearchType
+        Comparator<T> comparator = (Comparator<T>) map.get(Constants.COMPARATOR);
+        //initialize the searchConstant which is used as the terminating condition
+        int searchConstant = (int) map.get(Constants.SEARCH_CONSTANT);
 
-
-
+        //perform linear search
         while(i < nItems && comparator.compare((T) list.get(i), key) == searchConstant){
             i++;
         }
 
-        i = i -(int) map.get("subtract");
+        //DECREMENT the index returned, based on the decrement index value set for the corresponding search type
+        i = i -(int) map.get(Constants.DECREMENT_INDEX);
+
+        //if index is zero or greater than the amount of items in the list, return NotFound
         if(i == nItems || i < 0){
             result =  new Result(SearchResult.NotFound);
         }else{
+            //else get the search result by comparing the list index item and the key items value
             result = new Result(i, getSearchResult((T)list.get(i), key), list.get(i));
         }
 
---
+++ src/main/java/com/searchtype/utils/Constants.java
@@ -0,0 +1,11 @@
+package com.searchtype.utils;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: shriram
+ */
+public final class Constants {
+    public static final String COMPARATOR = "comparator";
+    public static final String SEARCH_CONSTANT = "searchConstant";
+    public static final String DECREMENT_INDEX = "decrementIndex";
+}
